71dbe2417150c44e172fcc41ed38a24fd0131624
Kamie <kamboydev@tuta.io>
2025-08-06 13:15:27 -0700

    get rid of useless code

--- index.js
+++ index.js
@@ -6,23 +6,6 @@
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("SMYW 💔");
 
-const btn = document.querySelector("button"); // Get the button from the page
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } },
-    "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => {
-    goto(f.dataset.file, f.dataset.line);
-  };
-});
-
 // Website Redirects
 
 function projects() {
